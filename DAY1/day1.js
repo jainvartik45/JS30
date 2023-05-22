b0a5fed7f8e490a7562864747cc876be8b3c9c4e
jainvartik45 <84932356+jainvartik45@users.noreply.github.com>
2023-05-23 01:06:49 +0530

    Day1 Done

---
+++ DAY1/day1.js
@@ -0,0 +1,33 @@
+function mySound1() {
+    
+    
+
+    let mySound = new Audio('audio1.mp3')
+    mySound.play()
+}
+
+function mySound2() {
+    let mySound = new Audio('audio7.mp3')
+    mySound.play()
+}
+
+  function mySound3() {
+    let mySound = new Audio('audio3.wav')
+    mySound.play()
+  }
+
+  function mySound4() {
+    let mySound = new Audio('audio4.wav')
+    mySound.play()
+  }
+
+  function mySound5() {
+    let mySound = new Audio('audio5.wav')
+    mySound.play()
+  }
+
+  function mySound6() {
+    let mySound = new Audio('audio6.wav')
+    mySound.play()
+  }
+
